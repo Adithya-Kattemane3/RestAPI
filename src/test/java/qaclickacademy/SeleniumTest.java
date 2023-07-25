4ccb7d23feec84a4a39a270aee79fb4f76c79b80
adithya <adithyakattemane@gmail.com>
2023-07-25 12:59:58 +0530

    GitY commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		System.out.println("Web Automation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+		
+	}
+
+}
